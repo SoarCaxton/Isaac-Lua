2547304c99be1043cbad73cf8c865f07b7661acc
Keye3Tuido <lonely.stars@qq.com>
2025-12-15 11:25:12 +0800

    删除$CW，精确代码描述

--- 0.实用代码合辑.lua
+++ 0.实用代码合辑.lua
@@ -10,8 +10,9 @@ l local A,M,T=Isaac.AddCallback,ModCallbacks,{}A(T,M.XXX,func,arg)
 --10<=回调数N | 总字数=65+5N
 l local M,A=ModCallbacks,function(...)Isaac.AddCallback({},...)end;A(M.XXX,func,arg)
 
---0. 删除匿名模组的回调。游戏胜利后自动清理代码效果。
---控制台输入 lua CLM() 可删除所有匿名模组的回调，用于预防重复输入代码和清理代码效果。
+--0. 删除匿名模组的回调，用于预防重复输入代码和清理代码效果。
+--游戏胜利后自动清理代码效果。
+--控制台输入 lua CLM() 可手动删除所有匿名模组的回调，
 --重复输入此代码不额外生效。
 l local A,I,M,t,m=ModCallbacks,Isaac,'Mod'function CLM()for i,j in pairs(A)do t=I.GetCallbacks(j)for x=#t,1,-1 do m=t[x][M]if not(m and m.Name)then I.RemoveCallback(m,j,t[x].Function)end end end end CLM()I.AddCallback({},A.MC_POST_GAME_END,function(_,f)if not f then CLM()end end)
 
--- 12.永远迷失.lua
+++ 12.永远迷失.lua
@@ -6,8 +6,9 @@
 
 ---- 代码效果(不用管中文，全选复制即可) ----
 
---0. 删除匿名模组的回调。游戏胜利后自动清理代码效果。
---控制台输入 lua CLM() 可删除所有匿名模组的回调，用于预防重复输入代码和清理代码效果。
+--0. 删除匿名模组的回调，用于预防重复输入代码和清理代码效果。
+--游戏胜利后自动清理代码效果。
+--控制台输入 lua CLM() 可手动删除所有匿名模组的回调，
 --重复输入此代码不额外生效。
 l local A,I,M,t,m=ModCallbacks,Isaac,'Mod'function CLM()for i,j in pairs(A)do t=I.GetCallbacks(j)for x=#t,1,-1 do m=t[x][M]if not(m and m.Name)then I.RemoveCallback(m,j,t[x].Function)end end end end CLM()I.AddCallback({},A.MC_POST_GAME_END,function(_,f)if not f then CLM()end end)
 
